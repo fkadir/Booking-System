673b8e944bcce14350e11ddc3666320951822236
fkadir <99030827+fkadir@users.noreply.github.com>
2024-02-13 19:32:18 +0000

    try again

---
+++ backend/src/main/java/org/bookings/AppConfig.java
@@ -0,0 +1,4 @@
+package org.bookings;
+
+public class AppConfig {
+}
--- backend/src/main/java/org/bookings/config/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.bookings.config;
-
-public interface Constants {
-
-    // Change ???
-    public static final String JWT_KEY ="secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsd";
-    public static final String JWT_HEADER = "Authorization";
-}
